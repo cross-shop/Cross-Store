912fa26dd6525eccf84a5d9e407ae2ec4820e1e6
alisher <alisermanasov00@gmail.com>
2024-09-18 16:12:37 +0600

    dfghjk

--- src/components/Home/HomeCom.jsx
+++ src/components/Home/HomeCom.jsx
@@ -36,17 +36,17 @@ function HomeCom() {
   return (
     <div>
       <div className='top1'>
-      <div className='top11'>
-            <img src={sleva} alt="" />
-            <p>Гарантия лучшей цены</p>
-            <img src={prava} alt="" />
+        <div className='top11'>
+          <img src={sleva} alt="" />
+          <p>Гарантия лучшей цены</p>
+          <img src={prava} alt="" />
         </div>
 
       </div>
-       
-        <header className='header1'>
-          <img src={mainimage} alt="" />
-          <div className='header1gg'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, rerum!</p>
+      <header className='header1'>
+        <img src={mainimage} alt="" />
+        <div className='header1gg'>
           <div className='header1-left container'>
             <ul>
               <li>
@@ -68,21 +68,21 @@ function HomeCom() {
             </ul>
           </div>
           <div className='header1-img'>
-          <img src={Like} alt="" />
-          <img src={Magazin} alt="" />
-          <img src={search} alt="" />
+            <img src={Like} alt="" />
+            <img src={Magazin} alt="" />
+            <img src={search} alt="" />
           </div>
-          </div>
+        </div>
 
-          <div className='header1-center'>
-            <h1>распродажа <br />
+        <div className='header1-center'>
+          <h1>распродажа <br />
             товаров для неё</h1>
-            <button>Перейти в каталог</button>
-          </div>
+          <button>Перейти в каталог</button>
+        </div>
 
-          <div className='main1-status'>
+        <div className='main1-status'>
           <div className='status'>
-        <div className='status1'>
+            <div className='status1'>
               <img src={status1} alt="" />
               <p>покупка</p>
             </div> <div className='status1'>
@@ -101,345 +101,345 @@ function HomeCom() {
               <img src={status6} alt="" />
               <p>покупка</p>
             </div>
-            </div>
-
           </div>
-      
-        </header>
 
-        <main className='main1 container'>
-          <div className='main1-brend1'>
-    <h1>Cамое популярное сейчас</h1>
-    <button>Перейти</button>
-          </div>
-          <div className='main1-kros'>
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros1} alt="" />
-    
-              </div>
-              <div className='pp'>NOT</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* .... */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros2} alt="" />
-    
-              </div>
-              <div className='pp'>NOT</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* // */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros1} alt="" />
-    
-              </div>
-              <div className='pp'>NOT</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* // */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros2} alt="" />
-    
-              </div>
-              <div className='pp'>NOT</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-
-</div>
-<div className='main1-brend1-2'>
-    <h1>Новинки</h1>
-    <button>Перейти</button>
-          </div>
-          <div className='main1-kros'>
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros3} alt="" />
-    
-              </div>
-              <div className='pp-1'>pre-loved</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* .... */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros2} alt="" />
-    
-              </div>
-              <div className='pp-2'>NEW</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* // */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros1} alt="" />
-    
-              </div>
-              <div className='pp-2'>-40%</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* // */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros4} alt="" />
-    
-              </div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-
-</div>
-
-      <div className='main1-odezda1'>
-      <div className='odezda-1'>
-        <img src={odezda} alt="" />
-        <p>одежда</p>
-      </div>
-      <div className='odezda-1'>
-        <img src={odezda2} alt="" />
-        <p>обувь</p>
-      </div>
-      <div className='odezda-3'>
-        <div className='odezda-3-1'>
-          <img src={odezda3} alt="" />
-          <p>аксессуары</p>
         </div>
-        <div className='odezda-3-1'>
-          <img src={odezda4} alt="" />
-          <p>сумки</p>
+
+      </header>
+
+      <main className='main1 container'>
+        <div className='main1-brend1'>
+          <h1>Cамое популярное сейчас</h1>
+          <button>Перейти</button>
         </div>
-        <div className='odezda-3-1'>
-          <img src={odezda5} alt="" />
-          <p>для спорта</p>
-        </div>
-      </div>
-      </div>
+        <div className='main1-kros'>
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros1} alt="" />
 
-      <div className='main1-brend1-3'>
-    <h1>Для вечерних прогулок</h1>
-    <button>Перейти</button>
-          </div>
-          <div className='main1-kros'>
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros3} alt="" />
-    
-              </div>
-              <div className='pp-1'>pre-loved</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
+            </div>
+            <div className='pp'>NOT</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
             <div className='main-top1'>
-              <p>Сумка Jacquemus 
+              <p>Сумка Jacquemus
               </p>
               <p>Le Bambidou</p>
               <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* .... */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros2} alt="" />
-    
-              </div>
-              <div className='pp-2'>NEW</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* // */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros1} alt="" />
-    
-              </div>
-              <div className='pp-2'>-40%</div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
-            </div>
-            {/* // */}
-            <div className='kros1'>
-              <div className='mm'>
-            <img src={kros4} alt="" />
-    
-              </div>
-              <div className='pw1'>
-                <img src={Like2} alt="" />
-              </div> <br />
-    
-            <div className='main-top1'>
-              <p>Сумка Jacquemus 
-              </p>
-              <p>Le Bambidou</p>
-              <h5>21 999₽</h5>
-            </div> 
-              
-
             </div>
 
-</div>
-
-      <div className='main1-Nike1'>
-        <p>Взгляните на</p>
-        <div className='Nike1'>
-          <div className='Nike1-1'>
-            <h2></h2>
-            <img src={sleva1} alt="" />
 
           </div>
-          <h1>NIKE</h1>
+          {/* .... */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros2} alt="" />
+
+            </div>
+            <div className='pp'>NOT</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
 
 
-          <div className='Nike1-2'>
-            <img src={prava1} alt="" />
-            <h2></h2>
+          </div>
+          {/* // */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros1} alt="" />
 
+            </div>
+            <div className='pp'>NOT</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+          {/* // */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros2} alt="" />
+
+            </div>
+            <div className='pp'>NOT</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+
+        </div>
+        <div className='main1-brend1-2'>
+          <h1>Новинки</h1>
+          <button>Перейти</button>
+        </div>
+        <div className='main1-kros'>
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros3} alt="" />
+
+            </div>
+            <div className='pp-1'>pre-loved</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+          {/* .... */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros2} alt="" />
+
+            </div>
+            <div className='pp-2'>NEW</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+          {/* // */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros1} alt="" />
+
+            </div>
+            <div className='pp-2'>-40%</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+          {/* // */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros4} alt="" />
+
+            </div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+
+        </div>
+
+        <div className='main1-odezda1'>
+          <div className='odezda-1'>
+            <img src={odezda} alt="" />
+            <p>одежда</p>
+          </div>
+          <div className='odezda-1'>
+            <img src={odezda2} alt="" />
+            <p>обувь</p>
+          </div>
+          <div className='odezda-3'>
+            <div className='odezda-3-1'>
+              <img src={odezda3} alt="" />
+              <p>аксессуары</p>
+            </div>
+            <div className='odezda-3-1'>
+              <img src={odezda4} alt="" />
+              <p>сумки</p>
+            </div>
+            <div className='odezda-3-1'>
+              <img src={odezda5} alt="" />
+              <p>для спорта</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className='main-mixer'>
+        <div className='main1-brend1-3'>
+          <h1>Для вечерних прогулок</h1>
+          <button>Перейти</button>
+        </div>
+        <div className='main1-kros'>
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros3} alt="" />
 
-<div className='mixer1'>
-  <img src={mixer1} alt="" />
-  <p>bred Fr <br /> mixer </p>
-  <div className='mixer1-img'>
-    <img src={prava1} alt="" />
-  </div>
-</div>
+            </div>
+            <div className='pp-1'>pre-loved</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
 
-<div className='mixer2'>
-  <img src={mixer2} alt="" />
-  <p>nike air <br /> jordan 1 </p>
-  <div className='mixer1-img'>
-    <img src={prava1} alt="" />
-  </div>
-</div>
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
 
-<div className='mixer1'>
-  <img src={mixer3} alt="" />
-  <p> nike air <br /> Maximus </p>
-  <div className='mixer1-img'>
-    <img src={prava1} alt="" />
-  </div>
-</div>
 
-      </div>
-          
+          </div>
+          {/* .... */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros2} alt="" />
 
-        </main>
+            </div>
+            <div className='pp-2'>NEW</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+          {/* // */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros1} alt="" />
+
+            </div>
+            <div className='pp-2'>-40%</div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+          {/* // */}
+          <div className='kros1'>
+            <div className='mm'>
+              <img src={kros4} alt="" />
+
+            </div>
+            <div className='pw1'>
+              <img src={Like2} alt="" />
+            </div> <br />
+
+            <div className='main-top1'>
+              <p>Сумка Jacquemus
+              </p>
+              <p>Le Bambidou</p>
+              <h5>21 999₽</h5>
+            </div>
+
+
+          </div>
+
+        </div>
+
+        <div className='main1-Nike1'>
+          <p>Взгляните на</p>
+          <div className='Nike1'>
+            <div className='Nike1-1'>
+              <h2></h2>
+              <img src={sleva1} alt="" />
+
+            </div>
+            <h1>NIKE</h1>
+
+
+            <div className='Nike1-2'>
+              <img src={prava1} alt="" />
+              <h2></h2>
+
+            </div>
+          </div>
+        </div>
+
+        <div className='main-mixer'>
+
+          <div className='mixer1'>
+            <img src={mixer1} alt="" />
+            <p>bred Fr <br /> mixer </p>
+            <div className='mixer1-img'>
+              <img src={prava1} alt="" />
+            </div>
+          </div>
+
+          <div className='mixer2'>
+            <img src={mixer2} alt="" />
+            <p>nike air <br /> jordan 1 </p>
+            <div className='mixer1-img'>
+              <img src={prava1} alt="" />
+            </div>
+          </div>
+
+          <div className='mixer1'>
+            <img src={mixer3} alt="" />
+            <p> nike air <br /> Maximus </p>
+            <div className='mixer1-img'>
+              <img src={prava1} alt="" />
+            </div>
+          </div>
+
+        </div>
+
+
+      </main>
     </div>
   )
 }

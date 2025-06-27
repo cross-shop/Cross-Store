119c688856f27bf509410f2b79653a5d8e999588
MacBook <macbook@MacBook-Air-MacBook.local>
2025-06-27 14:36:08 +0600

    commit name-components

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Zakaz from "../components/Payment";
+
+function Oferzakaz() {
+  return (
+    <div>
+      <Zakaz />
+    </div>
+  );
+}
+
+export default Oferzakaz;
